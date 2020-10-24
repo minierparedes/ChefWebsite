4dce754eeb4d4da57bed23bd8f19e27d234712ed
minierparedes <3thancr0wn@gmail.com>
2020-10-25 00:53:10 +0900

    Updated: Router routes: Products, Services, SignUp.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import Navbar from "./Components/Navbar";
 import { BrowserRouter  as Router, Switch, Route } from "react-router-dom" 
 import './App.css';
-import Home from "./Components/pages/home";
+import Home from "./Components/pages/Home";
+import Products from './Components/pages/Products';
+import Services from './Components/pages/Services';
+import SignUp from './Components/pages/SignUp';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
     <Navbar />
     <Switch>
     <Route path="/" exact component={Home}/>
+    <Route path="/services" component={Services}/>
+    <Route path="/products" component={Products}/>
+    <Route path="/sign-up" component={SignUp}/>
     </Switch>
     </Router>
     </>
