496a51ac376e4be1a8fca3581a527872792b8ce8
minierparedes <3thancr0wn@gmail.com>
2020-10-27 01:16:03 +0900

    Updated: Footer.css

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -11,25 +11,65 @@ function Footer() {
                 <p className="footer-subscription-text">Unsub</p>
                 <div className="input-area">
                     <form>
-                        <input type="email" name="email" placeholder="email@dot.com" className="footer-input"/>
+                        <input type="email" name="email" placeholder="email@dot.com" className="footer-input" />
                         <Button buttonStyle="btn--outline">Subscribe</Button>
                     </form>
                 </div>
             </section>
-                <section className="social-media">
-                    <div className="social-media-wrapper">
-                        <div className="footer-logo">
-                            <Link className="social-logo" to="/">Chef awesome logo<i /></Link>
-                        </div>
-                        <small className="website-rights">&copy;{new Date().getFullYear()} CHEF AWESOME | All rights reserved</small>
-                        <div className="social-icons">
-                            <Link className="social-icons-link linkedin" to="/" target="_blank" aria-label="LinkedIn">
-                                <i className="fab fa-linkedin"/>
-                            </Link>
-                        </div>
+            <div className="footer-links">
+                <div className="footer-links-wrapper">
+                    <div className="footer-links-items">
+                        <h2>About Chef awesome</h2>
+                        <Link to="/services">What I am up to</Link>
+                        <Link to="/services">Testimonials</Link>
+                        <Link to="/services">Terms of services</Link>
                     </div>
-                </section>
-            
+                    <div className="footer-links-items">
+                        <h2>Contact chef awesome</h2>
+                        <Link to="/services">Contact</Link>
+                        <Link to="/services">Support</Link>
+                        <Link to="/services">Events</Link>
+                    </div>
+                </div>
+                <div className="footer-links-wrapper">
+                    <div className="footer-links-items">
+                        <h2>?????</h2>
+                        <Link to="/services">????</Link>
+                        <Link to="/services">????</Link>
+                        <Link to="/services">????</Link>
+                    </div>
+                    <div className="footer-links-items">
+                        <h2>Social Media</h2>
+                        <Link to="/services">Instagram</Link>
+                        <Link to="/services">Facebook</Link>
+                        <Link to="/services">Twitter</Link>
+                        <Link to="/services">Line</Link>
+                    </div>
+                </div>
+            </div>
+            <section className="social-media">
+                <div className="social-media-wrapper">
+                    <div className="footer-logo">
+                        <Link className="social-logo" to="/">@<i /></Link>
+                    </div>
+                    <small className="website-rights">&copy;{new Date().getFullYear()} CHEF AWESOME | All rights reserved</small>
+                    <div className="social-icons">
+                        <Link className="social-icons-link facebook" to="/" target="_blank" aria-label="Facebook">
+                            <i className="fab fa-facebook-f" />
+                        </Link>
+                        <Link className="social-icons-link instagram" to="/" target="_blank" aria-label="Instagram">
+                            <i className="fab fa-instagram" />
+                        </Link>
+                        <Link className="social-icons-link twitter" to="/" target="_blank" aria-label="Twitter">
+                            <i className="fab fa-twitter" />
+                        </Link>
+                        <Link className="social-icons-link line" to="/" target="_blank" aria-label="Line">
+                            <i className="fab fa-line" />
+                        </Link>
+                    </div>
+                </div>
+            </section>
+
         </div>
     )
 }
