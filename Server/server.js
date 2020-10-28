478978afd8782e09e528ad7a8dfeac8737ea9022
minierparedes <3thancr0wn@gmail.com>
2020-10-29 02:24:47 +0900

    Updated: Recipes: Cors and useEffect axios fetch db successfully.

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const db = require("../Server/db")
 const morgan = require("morgan");
 
@@ -8,6 +9,7 @@ const app = express();
 
 
 app.use(express.json());
+app.use(cors());
 
 //GET all recipes
 app.get("/api/v1/recipes", async (req, res) => {
--- src/Components/pages/Recipes.js
+++ src/Components/pages/Recipes.js
@@ -16,7 +16,7 @@ export default function Recipes() {
 
     return (
         <div>
-
+            <h1>Hello world</h1>
         </div>
     )
 }
