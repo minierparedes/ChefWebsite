fd20a332db2c8fd97c5736df2b6e20c245ac116c
minierparedes <3thancr0wn@gmail.com>
2020-10-29 01:05:09 +0900

    Updated: server.js: RESTsfull methods & CRUD methods for PostgreSQL.

--- Server/server.js
+++ Server/server.js
@@ -12,60 +12,82 @@ app.use(express.json());
 //GET all recipes
 app.get("/api/v1/recipes", async (req, res) => {
     try {
-        const results = await db.query("select * from recipes");
+        const results = await db.query("SELECT * FROM recipes");
         res.status(200).json({
-        status: "success",
-        results: results.rowCount,
-        data: {
-            recipes: results.rows
-        }
-    }); 
+            status: "success",
+            results: results.rowCount,
+            data: {
+                recipes: results.rows
+            }
+        });
     } catch (error) {
         console.log(error);
     }
 });
 
 //GET recipe
-app.get("/api/v1/recipes/:id", (req, res) => {
-    console.log(req.params);
-    res.status(200).json({
-        status: "success",
-        data: {
-            recipe: "Enchiladas"
-        }
-    });
+app.get("/api/v1/recipes/:id", async (req, res) => {
+
+    try {
+        const results = await db.query("SELECT * FROM recipes WHERE recipe_id= $1", [req.params.id]);
+        res.status(200).json({
+            status: "success",
+            data: {
+                recipe: results.rows[0]
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+
+
 });
 
 //CREATE recipe
-app.post("/api/v1/recipes", (req, res) => {
-    console.log(req.body);
+app.post("/api/v1/recipes", async (req, res) => {
 
-    res.status(201).json({
-        status: "success",
-        data: {
-            recipe: "Enchiladas"
-        }
-    });
+    try {
+        const results = await db.query("INSERT INTO recipes (title, ingredients, directions, video_url) values ($1, $2, $3, $4) returning *", [req.body.title, req.body.ingredients, req.body.directions, req.body.video_url])
+        console.log(results);
+        res.status(201).json({
+            status: "success",
+            data: {
+                recipe: results.rows[0]
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+
+
 });
 
 //UPDATE recipe
-app.put("/api/v1/recipes/:id", (req, res) => {
-    console.log(req.params.id);
-    console.log(req.body);
+app.put("/api/v1/recipes/:id", async (req, res) => {
 
-    res.status(200).json({
-        status: "success",
-        data: {
-            recipe: "Enchiladas"
-        }
-    });
+    try {
+        const results = await db.query("UPDATE recipes SET title = $1, ingredients = $2, directions = $3, video_url =$4 WHERE recipe_id = $5 returning *", [req.body.title, req.body.ingredients, req.body.directions, req.body.video_url, req.params.id]);
+        res.status(200).json({
+            status: "success",
+            data: {
+                recipe: results.rows[0]
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 //DELETE recipe
-app.delete("/api/v1/recipes/:id", (req, res) => {
-    res.status(204).json({
-        status: "success"
-    });
+app.delete("/api/v1/recipes/:id", async (req, res) => {
+    try {
+        const results = await db.query("DELETE FROM recipes WHERE recipe_id = $1", [req.params.id]);
+        res.status(204).json({
+            status: "success"
+        });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 const PORT = process.env.PORT || 3555;
