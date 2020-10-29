9f7ccfc9f23df28051e6a947a985641c86e95188
minierparedes <3thancr0wn@gmail.com>
2020-10-30 00:00:31 +0900

    Updated: RecipeDetails.js useEffect for RecipesAPI.js

--- src/Components/pages/RecipeDetails.js
+++ src/Components/pages/RecipeDetails.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import RecipesAPI from '../../API/RecipesAPI';
 import "../../App.css";
+import { RecipesContext } from '../../Context/RecipesContext';
 import Footer from '../Footer';
 
 export default function RecipeDetails() {
+    const { id } = useParams();
+    const { selectedRecipe, setSelectedRecipe } = useContext(RecipesContext);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await RecipesAPI.
+        };
+    }, []);
+
     return (
         <div>
             <h1 className="recipe-detials">HELLO WORLD</h1>
-           
+
         </div>
     )
 }
--- src/Context/RecipesContext.js
+++ src/Context/RecipesContext.js
@@ -4,8 +4,9 @@ export const RecipesContext = createContext();
 
 export const RecipesContextProvider = props => {
     const [recipes, setRecipes] = useState([]);
+    const [selectedRecipe, setSelectedRecipe] = useState([]);
     return (
-        <RecipesContext.Provider value={{recipes:recipes, setRecipes}} >
+        <RecipesContext.Provider value={{recipes, setRecipes, selectedRecipe, setSelectedRecipe}} >
             {props.children}
         </RecipesContext.Provider>
     )
