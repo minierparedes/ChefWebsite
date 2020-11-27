a877ad2030c2c3d07f7f3a6890e84996d75d2a0b
minierparedes <3thancr0wn@gmail.com>
2020-11-27 23:13:10 +0900

    Updated: RecipesAPI.js: multiple baseURL.

--- src/API/RecipesAPI.js
+++ src/API/RecipesAPI.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 
-export default axios.create({
+const RecipesAPI = axios.create({
     baseURL: "http://localhost:4555/api/v1/recipes"
 });
+
+const adminAPI = axios.create({
+    baseURL: "http://localhost:4555/api/v1/recipes"
+});
+
+export default { RecipesAPI, adminAPI };
--- src/Components/Admin/Register.js
+++ src/Components/Admin/Register.js
@@ -1,16 +1,26 @@
 import React, {useState} from 'react';
 
 function Register() {
-    const [inputs, setInputs] = useState({name: "", password: ""});
-    const { name, password } = inputs;
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+        try {
+            const response = await 
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
 
     return (
         <>
             <div className="container">
                 <h1 className="text-center my-5" >Register</h1>
-                <form>
-                    <input className="form-control my-4" type="text" name="name" placeholder="name" />
-                    <input className="form-control my-4" type="password" name="password" placeholder="password" />
+                <form onSubmit={handleSubmit}>
+                    <input value={name} onChange={e => setName(e.target.value)} className="form-control my-4" type="text" name="name" placeholder="name" />
+                    <input value={password} onChange={e => setPassword(e.target.value)} className="form-control my-4" type="password" name="password" placeholder="password" />
                     <button className="btn btn-success btn block" >submit</button>
                 </form>
             </div>
